1f245d8bdf99111b5269240c06ef49c02ef19bc2
ironpenguin222 <148375713+ironpenguin222@users.noreply.github.com>
2024-09-23 12:55:02 -0400

    Acceleration added

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -10,12 +10,20 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
     private Vector3 velocity = new Vector3(0.001f, 0f);
-    public float moveSpeed = 10;
+    public float accelerationtTime = 3;
+    public float deceleration = 0.95f;
+    public float maxSpeed = 30;
+    public float acceleration;
 
+    private void Start()
+    {
+        acceleration = maxSpeed / accelerationtTime;
+    }
     void Update()
     {
         transform.position = transform.position + velocity * Time.deltaTime;
         PlayerMovement();
+        Debug.Log(velocity.magnitude);
     }
 
     void PlayerMovement()
@@ -23,21 +31,32 @@ public class Player : MonoBehaviour
         float upthing = Input.GetAxisRaw("Vertical");
         if (upthing > 0)
         {
-            velocity += transform.up * moveSpeed  * Time.deltaTime;
+            velocity += Vector3.up * acceleration  * Time.deltaTime;
         }
         if (upthing < 0)
         {
-            velocity -= transform.up * moveSpeed * Time.deltaTime;
+            velocity -= Vector3.up * acceleration * Time.deltaTime;
         }
         float sideThing = Input.GetAxisRaw("Horizontal");
         if (sideThing > 0)
         {
-            velocity += transform.right * moveSpeed * Time.deltaTime;
+            velocity += Vector3.right * acceleration * Time.deltaTime;
         }
         if (sideThing < 0)
         {
-            velocity -= transform.right * moveSpeed * Time.deltaTime;
+            velocity -= Vector3.right * acceleration * Time.deltaTime;
         }
-    }
+
+        if(velocity.magnitude > maxSpeed)
+        {
+            velocity = velocity.normalized * maxSpeed;
+        }
+
+        //if (Input.GetAxisRaw("Vertical") == 0)
+        //{
+        //   velocity *= deceleration;
+        //}
+        }
+    
 
 }
