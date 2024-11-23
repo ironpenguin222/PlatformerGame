dff3437d0ba48e9b871a75435a5fd0511fa02bd3
ironpenguin222 <148375713+ironpenguin222@users.noreply.github.com>
2024-11-23 14:06:26 -0500

    Moving
    added some movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,30 +2,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public float speed = 5;
     public enum FacingDirection
     {
         left, right
     }
 
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        // The input from the player needs to be determined and
-        // then passed in the to the MovementUpdate which should
-        // manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+        Vector2 playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
         MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
-
+        rb.velocity = new Vector2 (playerInput.x * speed, rb.velocity.y);
     }
 
     public bool IsWalking()
