ae706b40910fc965394cc77ea2ef53c92f4eb825
ironpenguin222 <148375713+ironpenguin222@users.noreply.github.com>
2024-11-24 00:32:59 -0500

    Finished the movement
    Added movement modifiers to the player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
+using static PlayerController;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float speed = 5;
+    public Animator animator;
+    public float maxSpeed = 10f;
+    public float acceleration = 5f;
+    public float deceleration = 3f;
     public enum FacingDirection
     {
         left, right
     }
-
+    private FacingDirection facingDirection = FacingDirection.right;
 
     void Start()
     {
@@ -20,16 +24,47 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
         MovementUpdate(playerInput);
+        UpdateFacingDirection(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
-        rb.velocity = new Vector2 (playerInput.x * speed, rb.velocity.y);
+        if (playerInput.x != 0)
+        {
+            rb.AddForce(playerInput * acceleration, ForceMode2D.Force);
+
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+        }
+        else
+        {
+                rb.AddForce(-rb.velocity.normalized * deceleration);
+        }
+    }
+
+    private void UpdateFacingDirection(Vector2 input)
+    {
+        if (input.x > 0 && facingDirection == FacingDirection.right)
+        {
+            facingDirection = FacingDirection.left;
+            Flip();
+        }
+        else if (input.x < 0 && facingDirection == FacingDirection.left)
+        {
+            facingDirection = FacingDirection.right;
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 
     public bool IsWalking()
     {
-        return false;
+            return false;
     }
     public bool IsGrounded()
     {
